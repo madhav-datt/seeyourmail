5515e8efe38ee184ec498251ac7d5c1a1854b62f
madhav-datt <madhav.datt@hotmail.com>
2016-08-03 18:37:30 +0530

    add requirements file

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from distutils.core import setup
+
+setup(
+    name='seeyourmail',
+    packages=['seeyourmail'],
+    version='1.0',
+    description='A minimalistic and simplified command line email client',
+    author='Madhav Datt',
+    author_email='madhav.datt@hotmail.com',
+    url='https://github.com/madhav-datt/seeyourmail',
+    download_url='https://github.com/madhav-datt/seeyourmail/tarball/1.0',
+    keywords=['email', 'receive', 'receive mail', 'automatic', 'attachment'],
+    classifiers=[],
+)
