3a030401d82069c6c3fa56e02bcbeac6c43ede73
madhav-datt <madhav.datt@hotmail.com>
2016-08-04 21:35:58 +0530

    refactor pop retrieval to imaplib

--- seeyourmail/seeyourmail.py
+++ seeyourmail/seeyourmail.py
@@ -1,18 +1,18 @@
 #
-# SendMail Class - a wrapper around the SMTP Python Package
-# Send emails from @gmail.com addresses
+# A wrapper around the imaplib Python Package
+# Retrieve emails from @gmail.com addresses
 #
 # Copyright (C)   2016    Madhav Datt
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #
 
-import poplib
+import imaplib
 from email import parser
 from authenticate import connect_email_account, get_password
 
 # Email login credentials
 _username = None
-pop_conn = poplib.POP3_SSL('pop.gmail.com')
+m = imaplib.IMAP4_SSL("imap.gmail.com")
 
 
 def login(username, password=None):
@@ -29,17 +29,17 @@ def login(username, password=None):
     if password:
         connect_email_account(username, password)
 
-    pop_conn.user(_username)
-    pop_conn.pass_(get_password(_username))
+    # pop_conn.user(_username)
+    # pop_conn.pass_(get_password(_username))
 
 
-# Get messages from server:
-messages = [pop_conn.retr(i) for i in range(1, len(pop_conn.list()[1]) + 1)]
-
-# Concat message pieces:
-messages = ["\n".join(message[1]) for message in messages]
-
-# Parse message intom an email object:
-messages = [parser.Parser().parsestr(message) for message in messages]
-
-pop_conn.quit()
+# # Get messages from server:
+# messages = [pop_conn.retr(i) for i in range(1, len(pop_conn.list()[1]) + 1)]
+#
+# # Concat message pieces:
+# messages = ["\n".join(message[1]) for message in messages]
+#
+# # Parse message intom an email object:
+# messages = [parser.Parser().parsestr(message) for message in messages]
+#
+# pop_conn.quit()
