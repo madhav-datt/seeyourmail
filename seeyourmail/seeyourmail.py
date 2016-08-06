05ac9636377788cd5522f846dc7d7894e28b2dd2
madhav-datt <madhav.datt@hotmail.com>
2016-08-06 16:47:59 +0530

    refactor pop retrieval to imaplib

--- seeyourmail/seeyourmail.py
+++ seeyourmail/seeyourmail.py
@@ -8,7 +8,7 @@
 
 import imaplib
 import os
-from email import parser
+import email
 from authenticate import connect_email_account, get_password
 
 # Email login credentials
@@ -35,3 +35,29 @@ def login(username, password=None):
 
     # Select gmail mail box to retrieve emails from
     imap_conn.select("[Gmail]/All Mail")
+
+
+def getmail():
+    """
+
+    :return: list of emails retrieved according to selected parameters
+    """
+
+    # Filter emails based on IMAP rules
+    response, items = imap_conn.search(None, "ALL")
+    mail_items = items[0].split()
+
+    for email_id in mail_items:
+        # Fetch and parse entire email's contents
+        response, data = imap_conn.fetch(email_id, "(RFC822)")
+        email_body = data[0][1]
+        mail = email.message_from_string(email_body)
+
+        # Email contents and data dictionary
+        _mail_dict = {
+            'from': mail['from'],
+            'to': mail['to'],
+            'date': mail['date'],
+            'subject': mail['subject'],
+            'from'
+        }
