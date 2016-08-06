961fc0ee8492c0ad8db64d60c7ee208d111cd414
madhav-datt <madhav.datt@hotmail.com>
2016-08-06 20:43:21 +0530

    refactor pop retrieval to imaplib

--- seeyourmail/seeyourmail.py
+++ seeyourmail/seeyourmail.py
@@ -60,8 +60,37 @@ def getmail():
         sender = ' '.join(sender)[1:-1]
 
         attachment = None
-        content = None
-        if not mail.is_multipart():
+        content = ''
+
+        if mail.is_multipart():
+            for part in mail.walk():
+
+                # Skip containers/multipart objects
+                if part.is_multipart():
+                    continue
+
+                # is this part an attachment ?
+                if part.get('Content-Disposition') is None:
+                    continue
+
+                filename = part.get_filename()
+                counter = 1
+
+                # if there is no filename, we create one with a counter to avoid duplicates
+                if not filename:
+                    filename = 'part-%03d%s' % (counter, 'bin')
+                    counter += 1
+
+                att_path = os.path.join(detach_dir, filename)
+
+                # Check if its already there
+                if not os.path.isfile(att_path):
+                    # finally write the stuff
+                    fp = open(att_path, 'wb')
+                    fp.write(part.get_payload(decode=True))
+                    fp.close()
+
+        else:
             content = mail.get_payload()
 
         # Email contents and data dictionary
