9a78911af0f3bcc8c1883e8feeb08752919fd0de
madhav-datt <madhav.datt@hotmail.com>
2016-08-08 23:45:58 +0530

    add nlp process search criteria building

--- seeyourmail/seeyourmail.py
+++ seeyourmail/seeyourmail.py
@@ -48,7 +48,14 @@ def _nlp_process(search_cond):
     mail_box_list = ['INBOX', '[Gmail]', '[Gmail]/All Mail', '[Gmail]/Drafts', '[Gmail]/Important', '[Gmail]/Sent Mail',
                      '[Gmail]/Spam', '[Gmail]/Starred', '[Gmail]/Trash']
 
-    mail_type = ['ANSWERED', 'ALL', 'SEEN', 'UNSEEN', 'RECENT', 'OLD', 'NEW']
+    mail_type = ['ANSWERED', 'UNANSWERED', 'ALL', 'SEEN', 'UNSEEN', 'RECENT', 'OLD', 'NEW', 'DELETED']
+
+    mail_date = ['SENTON {date}', 'SENTSINCE {date}', 'SENTBEFORE {date}']
+
+    mail_field = ['FROM "{search_text}"', 'TO "{search_text}"', 'SUBJECT "{search_text}"', 'BODY "{search_text}"',
+                  'TEXT "{search_text}"']
+
+    mail_size = ['SMALLER {size}', 'LARGER {size}']
 
     if search_cond is None:
         return '[Gmail]/All Mail', 'ALL'
