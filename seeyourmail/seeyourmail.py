449a2006d6aab2421d7fc93e7010c7485b640558
madhav-datt <madhav.datt@hotmail.com>
2016-08-10 00:22:14 +0530

    update nlp parsing

--- seeyourmail/seeyourmail.py
+++ seeyourmail/seeyourmail.py
@@ -37,30 +37,6 @@ def login(username, password=None):
         connect_email_account(username, password)
 
 
-def _nlp_process(search_cond):
-    """
-    Build structured search criteria from constraints specified in the natural language input string
-    :param search_cond: natural language english input with email search criteria
-    :return: mail_box, gmail mail box to retrieve emails from
-    :return: search_criteria, structured search criteria string
-    """
-
-    mail_box_list = ['INBOX', '[Gmail]', '[Gmail]/All Mail', '[Gmail]/Drafts', '[Gmail]/Important', '[Gmail]/Sent Mail',
-                     '[Gmail]/Spam', '[Gmail]/Starred', '[Gmail]/Trash']
-
-    mail_type = ['ANSWERED', 'UNANSWERED', 'ALL', 'SEEN', 'UNSEEN', 'RECENT', 'OLD', 'NEW', 'DELETED']
-
-    mail_date = ['SENTON {date}', 'SENTSINCE {date}', 'SENTBEFORE {date}']
-
-    mail_field = ['FROM "{search_text}"', 'TO "{search_text}"', 'SUBJECT "{search_text}"', 'BODY "{search_text}"',
-                  'TEXT "{search_text}"']
-
-    mail_size = ['SMALLER {size}', 'LARGER {size}']
-
-    if search_cond is None:
-        return '[Gmail]/All Mail', 'ALL'
-
-
 def getmail(search_condition=None, dir_path='~/sym_data'):
     """
     Fetch, parse and arrange email contents from specified mail account according to inputted
